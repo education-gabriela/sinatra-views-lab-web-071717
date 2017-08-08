4d08e190a021eed3094ba839f2b5f340bf90867d
gabidavila <gabidavila@gmail.com>
2017-08-07 22:01:46 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,8 +1,18 @@
 class App < Sinatra::Base
+  get '/' do
+    erb :index
+  end
 
-	get '/' do
-		erb :index
-	end
+  get '/hello' do
+    erb :hello
+  end
 
+  get '/goodbye' do
+    name = "Joe"
+    erb :goodbye, :locals => {:name => name}
+  end
 
+  get '/date' do
+    erb :date
+  end
 end
